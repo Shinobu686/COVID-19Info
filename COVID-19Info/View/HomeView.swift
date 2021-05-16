2ded89d611dbb1bcc1b3f46c5770a2409a559e5e
Shinobu686 <choppu.business@gmail.com>
2021-05-16 20:42:52 +0900

    fix #11 WKWebViewの実装

--- COVID-19Info/View/HomeView.swift
+++ COVID-19Info/View/HomeView.swift
@@ -6,61 +6,72 @@
 //
 
 import SwiftUI
+import WKView
 
 struct HomeView: View {
     
     var screenWidth = UIScreen.main.bounds.width
+    
     @ObservedObject var covidInfoVM = CovidInfoViewModel()
+    @State var flag: Bool = false
     
     var body: some View {
         
-        VStack {
-            Text("新型コロナウイルスの状況")
-                .fontWeight(.bold)
-                .font(.title2)
-                .foregroundColor(.white)
-            
-            ZStack {
-                RoundedRectangle(cornerRadius: 30)
-                    .fill(Color.white)
-                    .frame(width: screenWidth, height: screenWidth + 100)
-                    .shadow(color: .gray, radius: 3, x: 10, y: 7)
-                
-                HStack(spacing: 10) {
-                    VStack(spacing: 40) { // 数値は仮
-                        CovidDataItem(itemName: "PCR検査数", numValue: "\(covidInfoVM.covidData?.pcr ?? 0)")
-                        
-                        CovidDataItem(itemName: "入院者数", numValue: "\(covidInfoVM.covidData?.hospitalize ?? 0)")
-                        CovidDataItem(itemName: "死者数", numValue: "\(covidInfoVM.covidData?.death ?? 0)")
-                    }
-                    
-                    VStack(spacing: 40) { // 数値は仮
-                        CovidDataItem(itemName: "感染者数", numValue: "\(covidInfoVM.covidData?.positive ?? 0)")
-                        CovidDataItem(itemName: "重症者数", numValue: "\(covidInfoVM.covidData?.severe ?? 0)")
-                        CovidDataItem(itemName: "退院者数", numValue: "\(covidInfoVM.covidData?.discharge ?? 0)")
-                    }
-                }.onAppear() {
-                    self.covidInfoVM.fetchCovidInfo()
-                }
-            }.padding(.bottom)
-            
-            Button(action: {}) {
-                Text("厚労省ホームページ")
+        NavigationView {
+            VStack {
+                Text("新型コロナウイルスの状況")
                     .fontWeight(.bold)
-                    .frame(width: screenWidth / 1.2, height: screenWidth / 9)
-                    .foregroundColor(Color.white)
                     .font(.title2)
-                    .background(Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)))
-                    .padding()
+                    .foregroundColor(.white)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 30)
+                        .fill(Color.white)
+                        .frame(width: screenWidth, height: screenWidth + 100)
+                        .shadow(color: .gray, radius: 3, x: 10, y: 7)
+                    
+                    HStack(spacing: 10) {
+                        VStack(spacing: 40) {
+                            CovidDataItem(itemName: "PCR検査数", numValue: "\(covidInfoVM.covidData?.pcr ?? 0)")
+                            
+                            CovidDataItem(itemName: "入院者数", numValue: "\(covidInfoVM.covidData?.hospitalize ?? 0)")
+                            CovidDataItem(itemName: "死者数", numValue: "\(covidInfoVM.covidData?.death ?? 0)")
+                        }
+                        
+                        VStack(spacing: 40) {
+                            CovidDataItem(itemName: "感染者数", numValue: "\(covidInfoVM.covidData?.positive ?? 0)")
+                            CovidDataItem(itemName: "重症者数", numValue: "\(covidInfoVM.covidData?.severe ?? 0)")
+                            CovidDataItem(itemName: "退院者数", numValue: "\(covidInfoVM.covidData?.discharge ?? 0)")
+                        }
+                    }.onAppear() {
+                        self.covidInfoVM.fetchCovidInfo()
+                    }
+                }.padding(.bottom)
+                
+                NavigationLink(destination: WebView(url: "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000164708_00001.html"), isActive: $flag) {
+                    EmptyView()
+                }
+                
+                Button(action: {
+                    self.flag.toggle()
+                }) {
+                    Text("厚労省ホームページ")
+                        .fontWeight(.bold)
+                        .frame(width: screenWidth / 1.2, height: screenWidth / 9)
+                        .foregroundColor(Color.white)
+                        .font(.title2)
+                        .background(Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)))
+                        .padding()
+                }
             }
+            .frame(minWidth: 0,
+                   maxWidth: .infinity,
+                   minHeight: 0,
+                   maxHeight: .infinity,
+                   alignment: .center)
+            .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 0.6177746357)))
+            .edgesIgnoringSafeArea(.all)
         }
-        .frame(minWidth: 0,
-               maxWidth: .infinity,
-               minHeight: 0,
-               maxHeight: .infinity,
-               alignment: .center)
-        .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 0.6177746357)))
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
